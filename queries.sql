19f217e4796154c51ff3e715bb7867bc6677e8e6
ishimwepati <wazacode@gmail.com>
2023-09-27 19:27:30 +0300

    Queries created

--- queries.sql
+++ queries.sql
@@ -1,3 +1,26 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+
+/* Find all animals whose name ends in "mon". */
+SELECT * from animals WHERE like 'mon';
+
+/*List the name of all animals born between 2016 and 2019. */
+SELECT name from animals WHERE date_of_birth between '2016-01-01' and '2019-12-31';
+
+/* List the name of all animals that are neutered and have less than 3 escape attempts.*/
+SELECT name from animals WHERE neutered = true and escape_attempts < 3;
+
+/*List the date of birth of all animals named either "Agumon" or "Pikachu". */
+SELECT date_of_birth FROM animals where (name='Agumon') OR (name='Pikachu');
+
+/* List name and escape attempts of animals that weigh more than 10.5kg */
+SELECT name,escape_attempts FROM animals where weight_kg > 10.5;
+
+/*Find all animals that are neutered.*/
+SELECT * from animals WHERE neutered = true;
+
+/*Find all animals not named Gabumon.*/
+SELECT * FROM animals WHERE name != 'Gabumon';
+
+/*Find all animals with a weight between 10.4kg and 17.3kg (including the animals with the weights that equals precisely 10.4kg or 17.3kg) */
+SELECT * from animals WHERE weight_kg between 10.4 and 17.3;
