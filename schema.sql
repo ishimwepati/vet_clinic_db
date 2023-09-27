bf8ed3bb8a5cd31e9427e3b4b7422290bb77ec81
ishimwepati <wazacode@gmail.com>
2023-09-27 14:23:32 +0300

    Schema Created

--- schema.sql
+++ schema.sql
@@ -1,5 +1,15 @@
 /* Database schema to keep the structure of entire database. */
+CREATE DATABASE vet_clinic
+    WITH
+    OWNER = wazacode
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.UTF-8'
+    LC_CTYPE = 'en_US.UTF-8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
 
+    
 CREATE TABLE animals (
     name varchar(100)
 );
