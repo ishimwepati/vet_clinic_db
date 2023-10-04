aa0305e02107e07ffe79fe0bce4d53d9311745b7
ishimwepati <wazacode@gmail.com>
2023-10-04 12:05:30 +0300

    Schema Based on diagram added

---
+++ schema_based_on_diagram.sql
@@ -0,0 +1,38 @@
+/* Database schema_based_on_diagram to keep the structure of entire database. */
+
+CREATE TABLE patients (
+    id serial PRIMARY KEY,
+    name varchar(255),
+    date_of_birth date
+);
+
+/* Creating a medical_histories table */
+CREATE TABLE medical_histories (
+    id serial PRIMARY KEY,
+    admitted_at timestamp,
+    patient_id int REFERENCES patients(id),
+    status varchar(255)
+);
+
+CREATE TABLE invoices (
+    id serial PRIMARY KEY,
+    total_amount decimal,
+    generated_at timestamp,
+    payed_at timestamp,
+    medical_history_id int REFERENCES medical_histories(id)
+);
+
+CREATE TABLE treatment (
+    medical_history_id int PRIMARY KEY REFERENCES medical_histories(id),
+    type varchar(255),
+    name varchar(255)
+);
+
+CREATE TABLE invoice_items (
+    id serial PRIMARY KEY,
+    unit_price decimal,
+    quantity int,
+    total_price decimal,
+    invoice_id int REFERENCES invoices(id),
+    treatment_id int REFERENCES treatment(id)
+);
